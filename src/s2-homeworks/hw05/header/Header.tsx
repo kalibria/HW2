5ea4f647301ec838513d38ae604af84f4eb60438
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-17 17:15:21 +0300

    task5

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -3,12 +3,16 @@ import burgerIcon from './burger.svg'
 import s from './Header.module.css'
 import {useLocation} from 'react-router-dom'
 import {PATH} from '../Pages'
+import s2 from "../../../s1-main/App.module.css";
+import {Sidebar} from "../sidebar/Sidebar";
 
 type PropsType = {
     handleOpen: () => void
+    handleClose: () => void
+    open: boolean
 }
 
-export const Header: FC<PropsType> = ({handleOpen}) => {
+export const Header: FC<PropsType> = ({handleOpen, open, handleClose}) => {
     // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
     const location = useLocation()
     const currentPath = location.pathname
@@ -22,17 +26,21 @@ export const Header: FC<PropsType> = ({handleOpen}) => {
                     ? 'Junior Plus'
                     : 'Error'
     return (
-        <>
-            <div id={'hw5-header'} className={s.header}>
-                <img
-                    src={burgerIcon}
-                    id={'hw5-burger-menu'}
-                    className={s.burgerMenuIcon}
-                    onClick={handleOpen}
-                    alt={'open menu'}
-                />
-                <h1>{pageName}</h1>
+        <div className={s.headerWrapper}>
+            <div className={s2.container}>
+                <div id={'hw5-header'} className={s.header}>
+                    {open ? <Sidebar open={open} handleClose={handleClose}/> :
+                    <img
+                        src={burgerIcon}
+                        id={'hw5-burger-menu'}
+                        className={s.burgerMenuIcon}
+                        onClick={handleOpen}
+                        alt={'open menu'}
+                    />
+                    }
+                    <h1>{pageName}</h1>
+                </div>
             </div>
-        </>
+        </div>
     )
 }

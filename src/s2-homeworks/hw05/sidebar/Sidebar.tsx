83b914b8524f2611f768920c5f052712b1323b1c
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-18 14:42:29 +0300

    code refactored

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({isActive})=>[isActive ? "active" : ""].join(" ")}
+                        className={({ isActive }) => (isActive ? s.active : s.link)}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({isActive})=>[isActive ? "active" : ""].join(" ")}
+                        className={({ isActive }) => (isActive ? s.active : s.link)}
                     >
                         Junior
                     </NavLink>
@@ -47,7 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({isActive})=>[isActive ? "active" : ""].join(" ")}
+                        className={({ isActive }) => (isActive ? s.active : s.link)}
                     >
                         Junior Plus
                     </NavLink>

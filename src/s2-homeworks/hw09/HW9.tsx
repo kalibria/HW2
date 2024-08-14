9aa37dd11c8653fca486716ede99d7aaa253f025
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-14 17:29:29 +0300

    completed the task 9

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -12,10 +12,15 @@ import s2 from '../../s1-main/App.module.css'
 const HW9 = () => {
     return (
         <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
-
-            <div className={s2.hw}>
-                <Clock />
+            <div className={s2.container}>
+                <div className={s2.hwTitle}>Homework #9</div>
+            </div>
+            <div className={s2.wrapper}>
+                <div className={s2.container}>
+                    <div className={s2.hw}>
+                        <Clock/>
+                    </div>
+                </div>
             </div>
         </div>
     )

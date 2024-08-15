ce78a9d9d1f9e07a3dcc8e2365665edd70f231da
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-15 14:50:27 +0300

    changed stringDate format

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -22,8 +22,6 @@ function Clock() {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
         clearInterval(timerId);
         setTimerId(undefined);
-        console.log(timerId)
-
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -33,13 +31,12 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = `${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}
-    :
-    ${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}
-    :
-    ${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()} ` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    // const stringDate = `${date.toLocaleString('en-GB', {dateStyle: "full"})} ` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-    const stringDate = `${date.toLocaleString('en-GB', {dateStyle: "short"})} ` || <br/>
+    const stringTime = `${date.getHours() < 10 ? '0' + date.getHours():date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes():date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds():date.getSeconds()} ` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+    const stringDate = `${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}.${date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}.${date.getFullYear()}` || <br/>;
+
+    console.log('stringDate', stringDate)
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 

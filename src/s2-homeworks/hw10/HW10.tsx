b3bee841ba395deeb201c13548c50bdd53cbfb1f
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-21 15:35:00 +0300

    completed task 10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,31 +15,40 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading);
+    const dispatch = useDispatch();
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
         <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
-
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
+            <div className={s2.container}>
+                <div className={s2.hwTitle}>Homework #10</div>
+            </div>
+            <div className={s2.wrapper}>
+                <div className={s2.container}>
+                    <div className={s2.hw}>
+                        {isLoading ? (
+                            <div id={'hw10-loading'}>
+                                <Loader/>
+                            </div>
+                        ) : (
+                            <SuperButton
+                                id={'hw10-button-start-loading'}
+                                onClick={setLoading}
+                            >
+                                Set loading...
+                            </SuperButton>
+                        )}
                     </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
+                </div>
             </div>
         </div>
     )
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,14 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+
+export const loadingReducer = (state = initState, action: ActionType): {isLoading: boolean} => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING':{
+            return {isLoading: action.isLoading}
+        }
 
         default:
             return state
@@ -15,6 +20,9 @@ type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
+type ActionType =
+    | LoadingActionType
+
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',

25a519189c3feb4260f558238114e78a8bce31bc
Maria Kazakova <mariakazakova92@gmail.com>
2024-09-12 15:11:33 +0300

    changed setText

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -42,30 +42,26 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок)\n' +
-                    'код 200 - обычно означает что скорее всего всё ок)')
+                setText(res.data.errorText + `\n` + res.data.info)
                 // дописать
 
 
             })
             .catch((e) => {
                 // дописать
-                console.log("error", e.response.status)
+                console.log("error", e)
                 if(e.response.status === 500){
                     setCode('Код 500!')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере\n' +
-                        'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText(e.response.data.errorText + '\n' + e.response.data.info)
                 }else if(e.response.status === 400){
                     setCode('Код 400!')
                     setImage(error400)
-                    setText('Ты не отправил success в body вообще!\n' +
-                        'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setText(e.response.data.errorText + '\n' + e.response.data.info)
                 }else {
                     setCode('Error')
                     setImage(errorUnknown)
-                    setText('Network Error\n' +
-                        'AxiosError')
+                    setText(e.message + '\n' +)
                 }
 
             })
